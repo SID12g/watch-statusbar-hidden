65965dc5cfb0964ef2ad411d169563dce888fefd
SID12g <sid12g@naver.com>
2024-09-12 21:42:24 +0900

    Initial Commit

---
+++ WatchStatusBarHidden Watch App/WatchStatusBarHiddenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchStatusBarHiddenApp.swift
+//  WatchStatusBarHidden Watch App
+//
+//  Created by 조성민 on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchStatusBarHidden_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
